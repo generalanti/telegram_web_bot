5f64176cf63b259397015ca232741559e90403c7
generalanti <generalanti@gmail.com>
2023-01-29 20:08:52 +0300

    add test logo add ЮKassa test payment Token and info

--- config.py
+++ config.py
@@ -1,3 +1,20 @@
 BOT_USERNAME = "Design Fury"
 BOT_NAME = "designfurybot"
 BOT_TOKEN = "5692554919:AAEDq6gU8MRWmUOq9YMWx429_DEgItAdYKo"
+PAYMENTS_TOKEN = "381764678:TEST:49420"
+
+
+#Принято! Ваши тестовые настройки ЮKassa:
+
+# - ЮKassa Test: 381764678:TEST:49420
+
+# shopId 506751
+# shopArticleId 538350
+# Здесь всё. Теперь возвращайтесь к @BotFather — он пришлет тестовый платежный токен.
+# Для оплаты используйте данные тестовой карты:
+#
+# 1111 1111 1111 1026, 12/22, CVC 000.
+#
+# Когда будете готовы принимать настоящие платежи, выберите у @BotFather своего бота,
+# затем Bot Settings -> Payments -> ЮKassa: платежи. Вам потребуется настоящий shopId,
+# его выдают после регистрации в ЮKassa: https://yookassa.ru/joinups
